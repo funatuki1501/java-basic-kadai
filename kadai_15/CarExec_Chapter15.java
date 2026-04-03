33849d81242d122eb765ae533a8bac04fb719308
funatuki1501 <funatuki1501@gmail.com>
2026-04-03 18:43:56 +0900

    Add files via upload

---
+++ kadai_15/CarExec_Chapter15.java
@@ -0,0 +1,10 @@
+package text.kadai_15;
+
+public class CarExec_Chapter15 {
+    public static void main(String[] args) {
+        Car_Chapter15 car = new Car_Chapter15();
+
+        car.changeGear(3);
+        car.run();
+        }
+}
